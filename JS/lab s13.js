6a1676a430cf5199aded06504797cb8e9c713018
hoangtu3010 <70878927+hoangtu3010@users.noreply.github.com>
2020-11-07 16:39:16 +0700

    labs14

--- JS/lab s13.js
+++ JS/lab s13.js
@@ -1,60 +1,4 @@
-var products = [
-    {
-        name:"Flying Ninja",
-        image:"imgs/lab s13.jpg",
-        price: 18,
-        star:3.5
-    },
-    {
-        name:"Flying Ninja",
-        image:"imgs/lab s13.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        name:"Flying Ninja",
-        image:"imgs/lab s13.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        name:"Flying Ninja",
-        image:"imgs/lab s13.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        name:"Flying Ninja",
-        image:"imgs/lab s13.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        name:"Flying Ninja",
-        image:"imgs/lab s13.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        name:"Flying Ninja",
-        image:"imgs/lab s13.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        name:"Flying Ninja",
-        image:"imgs/lab s13.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        name:"Flying Ninja",
-        image:"imgs/lab s13.jpg",
-        price: 12,
-        star:3.5
-    }
-]
-function gridProduct() {
+function gridProduct(products) {
     var g_html = "";
     for (var i=0;i<products.length;i++){
         g_html += "<div class=\"col-md-4\">\n" +
@@ -74,5 +18,19 @@ function gridProduct() {
 
     return g_html;
 }
-var grid = document.getElementById("grid-products");
-grid.innerHTML = gridProduct();
+function loadData(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function (){
+        if (this.readyState == 4 && this.status == 200){
+            var rs = this.responseText; // nhan text/json tu api
+            rs = JSON.parse(rs); // chuyen thanh object json
+            var products = rs.data;
+            // in html ra danh sach
+            var grid = document.getElementById("grid-products");
+            grid.innerHTML = gridProduct(products);
+        }
+    }
+    xhttp.open("GET", "https://foodgroup.herokuapp.com/api/today-special");
+    xhttp.send();
+}
+loadData();
